6627955e19aebc4e68c1328d7c3d8ae391bc86a4
Martyn McDonnell <mlmcdonnell@gmail.com>
2024-02-26 09:50:51 +1100

    LastStand: added double chance, updated rules and prize info

--- LastStand/LastStand/src/app/ladder/ladder.component.ts
+++ LastStand/LastStand/src/app/ladder/ladder.component.ts
@@ -7,6 +7,7 @@ interface IEntry {
   name: string;
   tips: number[];
   eliminatedRound: number;
+  doubleChanceRounds: number[];
   expand?: boolean;
 }
 
@@ -31,6 +32,8 @@ export class LadderComponent implements OnInit {
   competition$!: Observable<IModel>;
   Number = Number;
 
+  readonly allowedIncorrectRounds = 1;
+
   constructor(private state: LastStandStateService) { }
 
   ngOnInit(): void {
@@ -39,14 +42,18 @@ export class LadderComponent implements OnInit {
 
         const rounds = c.rounds.map(r => ({ teams: ['', r.homeTeam, r.awayTeam, 'draw'], result: r.result }));
 
-        const entries = c.entries.map(e => ({
-          name: e.name, tips: e.tips, expand: false,
-          eliminatedRound: this.getEliminatedRound(e.tips, rounds)
-        }));
+        const entries = c.entries.map(e => {
+          const elim = this.getEliminatedRound(e.tips, rounds);
+          return {
+            name: e.name, tips: e.tips, expand: false,
+            doubleChanceRounds: elim.doubleChance,
+            eliminatedRound: elim.eliminatedRound
+          };
+        });
 
         const model = {
           rounds: rounds,
-          entries: entries.sort((a,b) => a.name.localeCompare(b.name)).sort((a, b) => b.eliminatedRound - a.eliminatedRound)
+          entries: entries.sort((a, b) => a.name.localeCompare(b.name)).sort((a, b) => b.eliminatedRound - a.eliminatedRound)
         }
 
         return model;
@@ -54,13 +61,20 @@ export class LadderComponent implements OnInit {
     );
   }
 
-  private getEliminatedRound(tips: number[], rounds: IRound[]): number {
+  private getEliminatedRound(tips: number[], rounds: IRound[]): { doubleChance: number[]; eliminatedRound: number; } {
+    let doubleChanceRounds: number[] = [];
     for (let i = 0; i != rounds.length; i++) {
-      if (rounds[i].result !== 0 && tips[i] !== rounds[i].result && rounds[i].result !== 3) {
-        return i;
+      if (rounds[i].result !== 0 && tips[i] !== rounds[i].result && rounds[i].result <= 2) {
+        // check whether this entry crossed the threshold for incorrect tips
+        // before deciding if it was eliminated
+        if (doubleChanceRounds.length < this.allowedIncorrectRounds) {
+          doubleChanceRounds.push(i);
+        } else {
+          return { doubleChance: doubleChanceRounds, eliminatedRound: i };
+        }
       }
     }
 
-    return Number.MAX_SAFE_INTEGER;
+    return { doubleChance: doubleChanceRounds, eliminatedRound: Number.MAX_SAFE_INTEGER };
   }
 }
